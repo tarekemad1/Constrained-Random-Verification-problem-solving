6e4093504cb9e14fdbca79e1ffe1691d40726dab
Tarek Rayan <tarekrayyan50@gmail.com>
2024-06-24 13:25:42 +0300

    Problem 13 solution added

--- problem13.sv
+++ problem13.sv
@@ -5,6 +5,12 @@ program pr13;
             val dist {[0:100]:/70 ,
                                     [101:255]:/30} ;
          }
+         function void post_randomize;
+            if(val>= 0 && val <= 100)
+                $display("%d : in range 70 percentage" ,val);
+            else if (val>=101 && val <=255 ) 
+                $display("%d : in range 30 percentage ",val);
+         endfunction
     endclass
     initial begin 
         pattern tr  ; 
@@ -12,7 +18,6 @@ program pr13;
         repeat(10) begin 
             Randomization : assert (tr.randomize())
                 else $error("Assertion Randomization  failed!");
-            $display("%d",tr.val);
         end 
     end 
 endprogram
